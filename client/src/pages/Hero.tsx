9e8343abd559a340d579ddc865a969ddefab5023
Shaik Yaseen <hackercanhack69@gmail.com>
2024-12-09 14:30:39 +0530

    made changes

--- client/src/pages/Hero.tsx
+++ client/src/pages/Hero.tsx
@@ -1,3 +1,4 @@
+import { FeaturesSectionDemo } from "../components/ui/Box";
 import { WorldMapDemo } from "../components/ui/Map";
 import { TypewriterEffectDemo } from "../components/ui/TypeWriterEffectDemo";
 
@@ -5,10 +6,17 @@ const Hero = () => {
   return (
     <div className="w-full bg-black">
       <div className="w-full h-screen">
+        <p className="absolute top-5 left-5 text-white text-[30px] font-bold font-sans">
+          Glide
+        </p>
         <TypewriterEffectDemo />
       </div>
+
       <div className="w-full p-4">
         <WorldMapDemo />
+      </div>
+      <div className="w-full">
+        <FeaturesSectionDemo />
       </div>
     </div>
   );
