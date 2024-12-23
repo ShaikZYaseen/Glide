21d9ef273c165d9d81c599ad1299f6e6655c888f
Shaik Yaseen <hackercanhack69@gmail.com>
2024-12-24 00:02:29 +0530

    Add location input fields to HomePage component

--- client/src/components/ui/HomePage.tsx
+++ client/src/components/ui/HomePage.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Label } from "./Label";
+import { Input } from "./Input";
 
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full h-screen p-5 bg-black justify-center items-center flex ">
+      <div className="h-screen w-1/2 bg-black p-10">
+        <LabelInputContainer>
+          <Label className="ml-6" htmlFor="email">
+            Enter your location
+          </Label>
+          <Input
+            id="email"
+            className="pl-6"
+            type="email"
+            placeholder="Current location"
+          />
+        </LabelInputContainer>
+        <div className="w-1 h-[85px] bg-white absolute top-[85px] left-[370px]"></div>
+        <LabelInputContainer>
+          <Label className="ml-6" htmlFor="email">
+            Enter your destination location
+          </Label>
+          <Input
+            id="email"
+            type="email"
+            className="pl-6"
+            placeholder="Destination location"
+          />
+        </LabelInputContainer>
+      </div>
+    </div>
+  );
 };
+
+const LabelInputContainer = ({ children }: { children: React.ReactNode }) => (
+  <div className="mb-4 flex flex-col space-y-2">{children}</div>
+);
 
 export default HomePage;
