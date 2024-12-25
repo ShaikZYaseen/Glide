b74cd80e9e80e31538859cdee14e919b1583847c
Shaik Yaseen <hackercanhack69@gmail.com>
2024-12-25 13:46:58 +0530

    Add location input fields to HomePage component

--- client/src/components/ui/HomePage.tsx
+++ client/src/components/ui/HomePage.tsx
@@ -1,34 +1,71 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Label } from "./Label";
 import { Input } from "./Input";
+import { gsap } from "gsap";
 
 const HomePage = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [div, setDiv] = React.useState(false);
+
+  const handleFocus = () => {
+    setDiv(true);
+    gsap.to(containerRef.current, {
+      y: -window.innerHeight + 180,
+      duration: 0.8,
+      ease: "power3.out",
+    });
+  };
+
+  const handleBlur = () => {
+    setDiv(false);
+    gsap.to(containerRef.current, {
+      y: 0, // Reset the container to its original position
+      duration: 0.8,
+      ease: "power3.out",
+    });
+  };
+
   return (
-    <div className="w-full h-screen p-5 bg-black justify-center items-center flex ">
-      <div className="h-screen w-1/2 bg-black p-10">
-        <LabelInputContainer>
-          <Label className="ml-6" htmlFor="email">
-            Enter your location
-          </Label>
-          <Input
-            id="email"
-            className="pl-6"
-            type="email"
-            placeholder="Current location"
+    <div className="w-full h-screen bg-black  justify-center items-center flex flex-col relative">
+      {!div && (
+        <div className="w-full h-full object-contain">
+          <img
+            className="w-full h-full object-cover"
+            src="https://media.gettyimages.com/id/1268054405/vector/route-planning-city-driving-road-network-destination-map.jpg?s=612x612&w=0&k=20&c=fFAXAL-udAnyNMc03hDYeVl3fg7LJyGu3XG-6xPi58o="
+            alt=""
           />
-        </LabelInputContainer>
-        <div className="w-1 h-[85px] bg-white absolute top-[85px] left-[370px]"></div>
-        <LabelInputContainer>
-          <Label className="ml-6" htmlFor="email">
-            Enter your destination location
-          </Label>
-          <Input
-            id="email"
-            type="email"
-            className="pl-6"
-            placeholder="Destination location"
-          />
-        </LabelInputContainer>
+        </div>
+      )}
+
+      {div && <div className="w-full h-screen z-100 bg-zinc-800"></div>}
+      <div
+        ref={containerRef}
+        className="h-45 w-full bg-black p-4 absolute bottom-0 flex justify-center items-center text-sm"
+      >
+        <div className="w-2/3 p-3">
+          {" "}
+          <h1 className="text-white text-xl font-bold p-2">Find a trip</h1>
+          <LabelInputContainer>
+            <Input
+              id="email"
+              className="pl-6 h-[30px] w-full text-sm"
+              type="email"
+              placeholder="Current location"
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </LabelInputContainer>
+          <LabelInputContainer>
+            <Input
+              id="destination"
+              className="pl-6 h-[30px] w-full text-sm"
+              type="email"
+              placeholder="Destination location"
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </LabelInputContainer>
+        </div>
       </div>
     </div>
   );
