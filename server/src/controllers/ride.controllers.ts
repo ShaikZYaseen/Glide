5f9be388402012454b3e49ba9ea4c42c44b6c815
Shaik Yaseen <hackercanhack69@gmail.com>
2025-01-05 00:55:46 +0530

    fare route and controler

--- server/src/controllers/ride.controllers.ts
+++ server/src/controllers/ride.controllers.ts
@@ -3,6 +3,64 @@ import axios from "axios";
 import rideModels from "../models/ride.models";
 import crypto from "crypto";
 
+const fetchCoordinates = async (address: string): Promise<string> => {
+  const response = await axios.get(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+      address
+    )}.json`,
+    {
+      params: {
+        access_token: process.env.MAPBOX_API_KEY,
+      },
+    }
+  );
+
+  const { features } = response.data;
+  if (features && features.length > 0) {
+    const [longitude, latitude] = features[0].geometry.coordinates;
+    return `${longitude},${latitude}`;
+  } else {
+    throw new Error("No coordinates found for the address");
+  }
+};
+
+const fetchDistanceAndTime = async (
+  origin: string,
+  destination: string
+): Promise<{ distance: number; duration: number }> => {
+  const response = await axios.get(
+    `https://api.mapbox.com/directions/v5/mapbox/driving/${origin};${destination}.json`,
+    {
+      params: {
+        access_token: process.env.MAPBOX_API_KEY,
+      },
+    }
+  );
+  const { routes } = response.data;
+  if (routes && routes.length > 0) {
+    const { distance, duration } = routes[0];
+    return { distance, duration };
+  }
+
+  throw new Error("No routes found");
+};
+
+const calculateFare = (
+  distance: number,
+  duration: number,
+  rideType: "auto" | "car" | "bike"
+): number => {
+  const baseFare = { auto: 24, car: 40, bike: 16 };
+  const perKmRate = { auto: 8, car: 12, bike: 6.4 };
+  const perMinuteRate = { auto: 1.6, car: 2.4, bike: 1.2 };
+
+  return Math.round(
+    baseFare[rideType] +
+      (distance / 1000) * perKmRate[rideType] +
+      (duration / 60) * perMinuteRate[rideType]
+  );
+};
+
 export const createRide = async (
   req: Request,
   res: Response
@@ -26,64 +84,6 @@ export const createRide = async (
   }
 
   try {
-    const fetchCoordinates = async (address: string): Promise<string> => {
-      const response = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-          address
-        )}.json`,
-        {
-          params: {
-            access_token: process.env.MAPBOX_API_KEY,
-          },
-        }
-      );
-
-      const { features } = response.data;
-      if (features && features.length > 0) {
-        const [longitude, latitude] = features[0].geometry.coordinates;
-        return `${longitude},${latitude}`;
-      } else {
-        throw new Error("No coordinates found for the address");
-      }
-    };
-
-    const fetchDistanceAndTime = async (
-      origin: string,
-      destination: string
-    ): Promise<{ distance: number; duration: number }> => {
-      const response = await axios.get(
-        `https://api.mapbox.com/directions/v5/mapbox/driving/${origin};${destination}.json`,
-        {
-          params: {
-            access_token: process.env.MAPBOX_API_KEY,
-          },
-        }
-      );
-      const { routes } = response.data;
-      if (routes && routes.length > 0) {
-        const { distance, duration } = routes[0];
-        return { distance, duration };
-      }
-
-      throw new Error("No routes found");
-    };
-
-    const calculateFare = (
-      distance: number,
-      duration: number,
-      rideType: "auto" | "car" | "bike"
-    ): number => {
-      const baseFare = { auto: 24, car: 40, bike: 16 };
-      const perKmRate = { auto: 8, car: 12, bike: 6.4 };
-      const perMinuteRate = { auto: 1.6, car: 2.4, bike: 1.2 };
-
-      return Math.round(
-        baseFare[rideType] +
-          (distance / 1000) * perKmRate[rideType] +
-          (duration / 60) * perMinuteRate[rideType]
-      );
-    };
-
     // Fetch coordinates
     const originCoordinates = await fetchCoordinates(pickup as string);
     const destinationCoordinates = await fetchCoordinates(
@@ -125,6 +125,42 @@ export const createRide = async (
     res
       .status(500)
       .json({ error: "An error occurred while creating the ride" });
+  }
+};
+
+export const getFare = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { pickup, destination, vehicleType } = req.query;
+    if (!pickup || !destination || !vehicleType) {
+      res.status(400).json({
+        message: "Pickup, destination, and vehicle type are required",
+      });
+      return;
+    }
+    const originCoordinates = await fetchCoordinates(pickup as string);
+    const destinationCoordinates = await fetchCoordinates(
+      destination as string
+    );
+
+    // Get distance and time
+    const { distance, duration } = await fetchDistanceAndTime(
+      originCoordinates,
+      destinationCoordinates
+    );
+
+    // Calculate fare
+    const fare = calculateFare(
+      distance,
+      duration,
+      vehicleType as "auto" | "car" | "bike"
+    );
+
+    res.status(200).json({ fare });
+    return;
+  } catch (error) {
+    console.error("Error getting fare:", (error as Error).message);
+    res.status(500).json({ error: "An error occurred while getting the fare" });
+    return;
   }
 };
 
--- server/src/routes/ride.routes.ts
+++ server/src/routes/ride.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createRide } from "../controllers/ride.controllers";
+import { createRide, getFare } from "../controllers/ride.controllers";
 import { authMiddleware } from "../middlewares/auth.middlewares";
 
 const rideRouter = express.Router();
 
 rideRouter.route("/create-ride").post(authMiddleware, createRide);
+rideRouter.route("/get-fare").post(authMiddleware, getFare);
 
 export default rideRouter;
